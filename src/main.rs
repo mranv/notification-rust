6af222f7db73c015f96b4f7f619ac0aa87384286
Anubhav Gain <iamanubhavgain@gmail.com>
2024-04-30 13:59:13 +0530

    update: code only for linux based

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use notify_rust::Notification;
+Notification::new()
+    .summary("Firefox News")
+    .body("This will almost look like a real firefox notification.")
+    .icon("firefox")
+    .show()?;
+
+ // presistance program for notifications
+//     use notify_rust::{Notification, Hint};
+// Notification::new()
+//     .summary("Category:email")
+//     .body("This has nothing to do with emails.\nIt should not go away until you acknowledge it.")
+//     .icon("thunderbird")
+//     .appname("thunderbird")
+//     .hint(Hint::Category("email".to_owned()))
+//     .hint(Hint::Resident(true)) // this is not supported by all implementations
+//     .timeout(0) // this however is
+//     .show()?;
