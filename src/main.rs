871cd6598c289f379319dcc8186114a04d534eb3
mranv <iamanubhavgain@gmail.com>
2024-04-30 14:08:37 +0530

    Refactor main.rs to temporarily disable notifications and add code for persistent notifications

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
-use notify_rust::Notification;
-Notification::new()
-    .summary("Firefox News")
-    .body("This will almost look like a real firefox notification.")
-    .icon("firefox")
-    .show()?;
+ // temporary program for notifications
+
+// use notify_rust::Notification;
+
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     Notification::new()
+//         .summary("Firefox News")
+//         .body("This will almost look like a real firefox notification.")
+//         .icon("firefox")
+//         .show()?;
+    
+//     Ok(())
+// }
 
  // presistance program for notifications
-//     use notify_rust::{Notification, Hint};
-// Notification::new()
-//     .summary("Category:email")
-//     .body("This has nothing to do with emails.\nIt should not go away until you acknowledge it.")
-//     .icon("thunderbird")
-//     .appname("thunderbird")
-//     .hint(Hint::Category("email".to_owned()))
-//     .hint(Hint::Resident(true)) // this is not supported by all implementations
-//     .timeout(0) // this however is
-//     .show()?;
+ use notify_rust::{Notification, Hint};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    Notification::new()
+        .summary("Category:email")
+        .body("This has nothing to do with emails.\nIt should not go away until you acknowledge it.")
+        .icon("thunderbird")
+        .appname("thunderbird")
+        .hint(Hint::Category("email".to_owned()))
+        .hint(Hint::Resident(true)) // this is not supported by all implementations
+        .timeout(0) // this however is
+        .show()?;
+    
+    Ok(())
+}
